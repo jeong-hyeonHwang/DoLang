aef6608c0bcb77f7dfda17c1ea1c1c8637e06c95
임혁 <cs.javah@kakao.com>
2025-01-27 19:12:02 +0900

    build: authorization 서버 배포
    production.yml 생성
    DockerFile 생성
    users.sql 추가

--- module-core/src/main/java/live/dolang/core/domain/user/User.java
+++ module-core/src/main/java/live/dolang/core/domain/user/User.java
@@ -14,8 +14,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users", indexes = {
-        @Index(name = "idx_google_id", columnList = "google_id"),
-        @Index(name = "idx_email", columnList = "email")
+        @Index(name = "idx_users_google_id", columnList = "google_id", unique = true),
+        @Index(name = "idx_users_email", columnList = "email", unique = true),
 })
 public class User {
 
@@ -24,13 +24,13 @@ public class User {
     @Column(name = "user_id", columnDefinition = "INT")
     private Integer id;
 
-    @Column(name = "email", columnDefinition = "VARCHAR(100)", unique = true)
+    @Column(name = "email", columnDefinition = "VARCHAR(100)", unique = true, nullable = false)
     private String email;
 
-    @Column(name = "google_id", columnDefinition = "VARCHAR(255)", unique = true)
+    @Column(name = "google_id", columnDefinition = "VARCHAR(255)", unique = true, nullable = false)
     private String googleId;
 
-    @Column(name = "create_at", columnDefinition = "TIMESTAMP")
+    @Column(name = "create_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false)
     private LocalDateTime createAt;
 
 }
