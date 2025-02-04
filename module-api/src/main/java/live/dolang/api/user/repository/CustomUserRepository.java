ffd6b14c8c44960dc12305d1e322c4b1562b27cb
홍승찬 <hsch19@naver.com>
2025-02-04 18:10:21 +0900

    feat(api): 마이페이지 API 수정
    마이페이지 및 메인페이지에서 필요한 API 추가

--- module-api/src/main/java/live/dolang/api/user/repository/CustomUserRepository.java
+++ module-api/src/main/java/live/dolang/api/user/repository/CustomUserRepository.java
@@ -1,16 +1,21 @@
 package live.dolang.api.user.repository;
 
-
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import live.dolang.api.user.dto.ResponseUserInfoDto;
+import live.dolang.core.domain.tag.QTag;
+import live.dolang.core.domain.tag.Tag;
 import live.dolang.core.domain.user.QUser;
 import live.dolang.core.domain.user_language_level.QUserLanguageLevel;
 import live.dolang.core.domain.user_profile.QUserProfile;
 import live.dolang.core.domain.user_tag.QUserTag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
+@Repository
 public class CustomUserRepository {
     private final JPAQueryFactory queryFactory;
 
@@ -19,19 +24,41 @@ public class CustomUserRepository {
         QUserProfile profile = QUserProfile.userProfile;
         QUserLanguageLevel userLanguageLevel = QUserLanguageLevel.userLanguageLevel;
         QUserTag userTag = QUserTag.userTag;
+        QTag tag = QTag.tag;
+        //사용자 기본정보 + 프로필 조회
         ResponseUserInfoDto dto = queryFactory
                 .select(Projections.bean(
                         ResponseUserInfoDto.class,
                         profile.profileImageUrl.as("profileImage"),
                         profile.nickname.as("nickname"),
                         profile.countryId.as("countryId"),
-                        profile.nativeLanguageId.as("nativeLanguageId")
+                        profile.nativeLanguageId.as("nativeLanguageId"),
+                        profile.interestLanguageId.as("interestLanguageId")
                 ))
                 .from(user)
                 .join(user.userProfile, profile)
                 .where(user.id.eq(userId))
                 .fetchOne();
+        //TODO: dto null처리
 
-        return null;
+        //관심언어 레벨 조회(interestLanguageID를 조건으로 조회)
+        String interestLanguageLevelId = queryFactory
+                .select(userLanguageLevel.languageLevelId)
+                .from(userLanguageLevel)
+                .where(userLanguageLevel.user.id.eq(userId)
+                        .and(userLanguageLevel.languageId.eq(dto.getInterestLanguageId())))
+                .fetchOne();
+
+        //관심사 태그 조회
+        List<String> tags = queryFactory
+                .select(tag.name)
+                .from(userTag)
+                .join(userTag.tag,tag)
+                .where(userTag.user.id.eq(userId))
+                .fetch();
+
+        dto.setInterestLanguageId(interestLanguageLevelId);
+        dto.setTags(tags);
+        return dto;
     }
 }
