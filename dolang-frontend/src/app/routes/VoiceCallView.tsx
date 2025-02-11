6ea4fd7d92bdc82f50a22c4fcad18d964a1b5dba
isemae <deleted.lee13@gmail.com>
2025-02-11 15:50:42 +0900

    feat(call): 음성 통화 기본 view구현

--- dolang-frontend/src/app/routes/VoiceCallView.tsx
+++ dolang-frontend/src/app/routes/VoiceCallView.tsx
@@ -1,35 +1,39 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useStompClientContext } from '../../features/Matching/hooks/useClientContext.tsx';
+import { CallWaveIndicator } from '../../features/VoiceCall/components/CallWaveIndicator.tsx';
+import { usePeerContext } from '../../features/VoiceCall/hooks/usePeerContext.tsx';
 import { useCallContext } from '../../features/VoiceCall/hooks/useCallContext.tsx';
 
 function VoiceCallView() {
   const { matchingResult } = useStompClientContext();
   const { localStream, remoteStream, startCall, endCall } = useCallContext();
 
-  // 매칭 완료 후 바로 startCall을 자동 시작하고 싶다면:
-  useEffect(() => {
-    if (matchingResult) {
-      // 매칭 결과가 들어오면 오너 여부를 확인해서 바로 호출
-      startCall();
-    }
-  }, [matchingResult, startCall]);
-
+  const { peerId, callStatus, remotePeerId, audioRef } = usePeerContext();
   // 오디오 태그에 localStream, remoteStream을 바인딩
   return (
     <div>
-      <h1>Matching & Call Page</h1>
-      <button onClick={startCall}>Start Call (Offer 생성)</button>
-      <button onClick={endCall}>End Call</button>
+      <div className="call-participants-container">
+        <div>
+          <h2>Your Peer ID: {peerId}</h2>
+          <p>Status: {callStatus}</p>
+          <p>Remote Peer ID: {remotePeerId}</p>
+          <div>
+            <audio ref={audioRef} autoPlay controls />
+          </div>
+          <div></div>
+        </div>
+      </div>
 
       {/* 로컬 오디오 */}
+
       {localStream && (
-        <video
+        <audio
           autoPlay
           controls
           muted
-          ref={(video) => {
-            if (video && localStream) {
-              video.srcObject = localStream;
+          ref={(audio) => {
+            if (audio && localStream) {
+              audio.srcObject = localStream;
             }
           }}
         />
@@ -37,7 +41,7 @@ function VoiceCallView() {
 
       {/* 리모트 오디오 */}
       {remoteStream && (
-        <video
+        <audio
           autoPlay
           controls
           ref={(video) => {
