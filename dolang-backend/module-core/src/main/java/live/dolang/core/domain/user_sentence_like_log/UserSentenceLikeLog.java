c0a6904b98f1958a5df4592c68a0c80a066c456d
홍승찬 <hsch19@naver.com>
2025-02-11 18:14:45 +0900

    fix(core): created_at 컬럼 current_timestamp 속성 추가
    created_at 컬럼 current_timestamp 속성 추가

--- dolang-backend/module-core/src/main/java/live/dolang/core/domain/user_sentence_like_log/UserSentenceLikeLog.java
+++ dolang-backend/module-core/src/main/java/live/dolang/core/domain/user_sentence_like_log/UserSentenceLikeLog.java
@@ -1,6 +1,7 @@
 package live.dolang.core.domain.user_sentence_like_log;
 
 import jakarta.persistence.*;
+import live.dolang.core.domain.date_sentence.DateSentence;
 import live.dolang.core.domain.user.User;
 import live.dolang.core.domain.user_date_sentence.UserDateSentence;
 import lombok.AllArgsConstructor;
@@ -25,6 +26,10 @@ public class UserSentenceLikeLog {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "date_sentence_id", nullable = false)
+    private DateSentence dateSentence;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_date_sentence_id", nullable = false)
