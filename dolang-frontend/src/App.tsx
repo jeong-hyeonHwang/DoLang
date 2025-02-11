fdf25df88135c44245ce35b39f0ccaead9f01dd0
isemae <deleted.lee13@gmail.com>
2025-02-12 00:57:01 +0900

    revert: 병합 중 누락된 코드 복원

--- dolang-frontend/src/App.tsx
+++ dolang-frontend/src/App.tsx
@@ -3,17 +3,22 @@ import { Routes, Route } from 'react-router';
 import LayoutDesign from './shared/Layout.tsx';
 import LayoutNoNav from './shared/LayoutNoNav.tsx';
 import MainPage from './app/routes/MainView.tsx';
+import { StompClientProvider } from './features/Matching/hooks/useClientContext.tsx';
+import Layout from './shared/Layout.tsx';
+import MainView from './app/routes/MainView.tsx';
 import FeedView from './app/routes/FeedView.tsx';
 import SavedContentsView from './app/routes/SavedContentsView.tsx';
 import VoiceCallView from './app/routes/VoiceCallView.tsx';
 import UserProfileView from './app/routes/UserProfileView.tsx';
+import GoogleLoginView from './features/Auth/GoogleLoginView.tsx';
 import GoogleSignupView from './features/Auth/GoogleSignUpView.tsx';
 import GoogleLogin from './features/Auth/GoogleLogin.tsx';
 import SignupForm from './features/Auth/SignupForm.tsx';
 import { GlobalStyle } from './shared/globalStyle.tsx';
-
 import Bookmarks from './app/routes/SavedContents/Bookmark.tsx';
 import AudioFeed from './app/routes/SavedContents/AudioFeed.tsx';
+import { CallContextProvider } from './features/VoiceCall/hooks/useCallContext.tsx';
+import { PeerContextProvider } from './features/VoiceCall/hooks/usePeerContext.tsx';
 
 function App() {
   return (
@@ -22,10 +27,39 @@ function App() {
       <Routes>
         <Route element={<LayoutDesign />}>
           <Route index element={<MainPage />} />
+        {/* <Route element={<Layout />}> */}
+        <Route element={<Layout />}>
+          <Route
+            index
+            element={
+              <PeerContextProvider>
+                <StompClientProvider>
+                  <MainView />
+                </StompClientProvider>
+              </PeerContextProvider>
+            }
+          />
           <Route path="feed" element={<FeedView />} />
           <Route path="savedContents" element={<SavedContentsView />} />
           <Route path="profile" element={<UserProfileView />} />
           <Route path="/call" element={<VoiceCallView />} />
+          <Route
+            path="/call"
+            element={
+              <PeerContextProvider>
+                <StompClientProvider>
+                  <CallContextProvider>
+                    <VoiceCallView />
+                  </CallContextProvider>
+                </StompClientProvider>
+              </PeerContextProvider>
+            }
+          />
+
+          {/* Auth */}
+          <Route path="oauth2" element={<GoogleLoginView />} />
+          <Route path="oauth2/code" element={<GoogleLoginView />} />
+          <Route path="/signup" element={<GoogleSignupView />} />
 
           {/* savedContetns */}
           <Route path="savedContents/bookmark" element={<Bookmarks />} />
@@ -35,7 +69,6 @@ function App() {
           <Route path="guide" element={<>서비스 가이드</>} />
           {/* User */}
         </Route>
-
         {/* Auth */}
         <Route>
           <Route element={<LayoutNoNav />}>
