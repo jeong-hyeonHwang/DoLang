e8a9214077e7afb8352683a3855b74dc2e5945bc
임혁 <cs.javah@kakao.com>
2025-02-11 09:50:56 +0900

    fix(api): Cors 수정

--- dolang-backend/module-api/src/main/java/live/dolang/api/common/config/SecurityConfig.java
+++ dolang-backend/module-api/src/main/java/live/dolang/api/common/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request ->
                         request
                                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
