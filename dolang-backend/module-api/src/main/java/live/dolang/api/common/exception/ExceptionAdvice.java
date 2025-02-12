3551375f74edb9a11f6c72ef126a34a512d6cbaa
황정현 <hijihilin@gmail.com>
2025-02-12 10:10:23 +0900

    feat(api): 파라미터 Exception 추가

--- dolang-backend/module-api/src/main/java/live/dolang/api/common/exception/ExceptionAdvice.java
+++ dolang-backend/module-api/src/main/java/live/dolang/api/common/exception/ExceptionAdvice.java
@@ -3,6 +3,7 @@ package live.dolang.api.common.exception;
 import live.dolang.api.common.response.BaseResponse;
 import live.dolang.api.common.response.BaseResponseStatus;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -46,10 +47,21 @@ public class ExceptionAdvice {
         return BaseResponse.status(exception.getStatus());
     }
 
+    @ExceptionHandler(RequestParameterException.class)
+    public BaseResponse<BaseResponseStatus> requestParameterExceptionHandler(RequestParameterException exception) {
+        log.error("RequestParameterException has occurred. {} {} {}", exception.getMessage(), exception.getCause(), exception.getStackTrace()[0]);
+        return BaseResponse.status(exception.getStatus());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public BaseResponse<BaseResponseStatus> requestParameterExceptionHandler(MissingServletRequestParameterException exception) {
+        log.error("MissingServletRequestParameterException has occurred. {} {} {}", exception.getMessage(), exception.getCause(), exception.getStackTrace()[0]);
+        return BaseResponse.status(new RequestParameterException(BaseResponseStatus.INVALID_PARAMETER).getStatus());
+    }
+
     @ExceptionHandler(Exception.class)
     public BaseResponse<BaseResponseStatus> exceptionHandler(Exception exception) {
         log.error(exception.getMessage(), exception);
         return BaseResponse.status(BaseResponseStatus.INTERNAL_SERVER_ERROR);
     }
-
 }
