4121a922c0d2a467f54098680fa30a6248997b95
isemae <deleted.lee13@gmail.com>
2025-02-13 17:19:20 +0900

    refactor(components): 네임카드 컴포넌트 인자 수정 및 해당 컴포넌트를 사용하는 공통 컴포넌트 수정

--- dolang-frontend/src/shared/components/nameCard/NameCard.tsx
+++ dolang-frontend/src/shared/components/nameCard/NameCard.tsx
@@ -1,24 +1,8 @@
-import { useRecoilState } from 'recoil';
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { css } from '@emotion/react';
 import 'flag-icons/css/flag-icons.min.css';
 import { NameCardProps } from '../../types/NameCardProps.interface.ts';
-import { userState } from '../../../features/Auth/userState.ts';
-
-const NameCard = ({ userNickname, style }: NameCardProps) => {
-  const [user, setUser] = useRecoilState(userState);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const storedUser = sessionStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-    setLoading(false);
-  }, [setUser]);
-
-  if (loading) return <div>Loading...</div>;
-
+export const NameCard = ({ ...props }: NameCardProps) => {
   const nameCardStyle = css`
     display: flex;
     align-items: center;
@@ -26,7 +10,7 @@ const NameCard = ({ userNickname, style }: NameCardProps) => {
     height: 3.2rem;
     font-weight: semibold;
     font-size: 1.2rem;
-    border: ${style === 'compact' ? 'none' : style === 'bordered' ? '1px solid #cacaca' : 'none'};
+    border: ${props.style === 'compact' ? 'none' : props.style === 'bordered' ? '1px solid #cacaca' : 'none'};
     border-radius: 10px;
     position: relative;
     margin-bottom: 0.6rem;
@@ -40,12 +24,40 @@ const NameCard = ({ userNickname, style }: NameCardProps) => {
   `;
 
   const nameStyle = css`
-    ${style === 'compact' && 'display: none'};
+    ${props.style === 'compact' && 'display: none'};
     white-space: nowrap;
   `;
 
-  const userImageStyle = css`
-    background-color: ${user?.profileImageUrl ? 'transparent' : '#A133FF'};
+  return (
+    <div className="name-card" css={nameCardStyle}>
+      {props.userNickname && (
+        <div className="user-info" css={userInfoStyle}>
+          <UserImageWrapper
+            profileImageUrl={props.userImage}
+            userNickname={props.userNickname}
+            userCountry={props.userCountry}
+          />
+
+          <div css={nameStyle}>
+            <strong>{props.userNickname}</strong>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const UserImageWrapper = ({
+  profileImageUrl,
+  userNickname,
+  userCountry,
+}: {
+  profileImageUrl: string;
+  userNickname: string;
+  userCountry: string;
+}) => {
+  const userImageWrapperStyle = css`
+    background-color: ${profileImageUrl ? 'transparent' : '#A133FF'};
     display: flex;
     border-radius: 50%;
     width: 3rem;
@@ -62,50 +74,37 @@ const NameCard = ({ userNickname, style }: NameCardProps) => {
     box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.4);
   `;
 
+  const userImageStyle = css`
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+  `;
+
   return (
-    <div className="name-card" css={nameCardStyle}>
-      {user && (
-        <div className="user-info" css={userInfoStyle}>
-          <div className="user-image-wrapper" css={userImageStyle}>
-            {user.profileImageUrl ? (
-              <img
-                src={user.profileImageUrl}
-                alt="User profile"
-                css={css`
-                  width: 100%;
-                  height: 100%;
-                  border-radius: 50%;
-                  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
-                `}
-              />
-            ) : (
-              <div
-                css={css`
-                  width: 100%;
-                  height: 100%;
-                  background-color: #a133ff;
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-                  border-radius: 50%;
-                  color: white;
-                  font-weight: bold;
-                  font-size: 1.2rem;
-                  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
-                `}
-              >
-                {user.nickname?.charAt(0).toUpperCase()}
-              </div>
-            )}
-            {user.nationality && <span className={`fi fi-${user.nationality}`} css={flagStyle} />}
-          </div>
-          <div css={nameStyle}>
-            <strong>{user.nickname || userNickname}</strong>
-          </div>
+    <div className="user-image-wrapper" css={userImageWrapperStyle}>
+      {profileImageUrl ? (
+        <img src={profileImageUrl} alt="User profile" css={userImageStyle} />
+      ) : (
+        <div
+          css={css`
+            width: 100%;
+            height: 100%;
+            background-color: #a133ff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 50%;
+            color: white;
+            font-weight: bold;
+            font-size: 1.2rem;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+          `}
+        >
+          {userNickname?.charAt(0).toUpperCase()}
         </div>
       )}
+      {userCountry && <span className={`fi fi-${userCountry}`} css={flagStyle} />}
     </div>
   );
 };
-
-export default NameCard;
