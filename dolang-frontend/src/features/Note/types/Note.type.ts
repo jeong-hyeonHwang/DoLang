26b83dfa8b3803d1875b2c85c09dc9be746f9640
isemae <deleted.lee13@gmail.com>
2025-02-13 09:12:17 +0900

    refactor(note): 노트 타입 및 통신 메서드 변경

--- dolang-frontend/src/features/Note/types/Note.type.ts
+++ dolang-frontend/src/features/Note/types/Note.type.ts
@@ -1,7 +1,16 @@
-export type Note = {
+export interface NoteResponse {
+  createdAt: string;
+  id: string;
+  interestLanguageId: string;
+  interestNote: string;
+  nativeLanguageId: string;
+  nativeNote: string;
   userId: number;
+}
+
+export interface NoteRequest {
   nativeNote: string;
   interestNote: string;
   nativeLanguageId: string;
   interestLanguageId: string;
-};
+}
