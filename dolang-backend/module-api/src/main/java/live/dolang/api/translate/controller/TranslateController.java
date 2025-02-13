fcbaad7a00e50b9248abdc2aa46c5f9df1a7c785
이준환 <dlwnsghks0401@gmail.com>
2025-02-13 16:09:30 +0900

    fix(api): Translate swagger 수정

--- dolang-backend/module-api/src/main/java/live/dolang/api/translate/controller/TranslateController.java
+++ dolang-backend/module-api/src/main/java/live/dolang/api/translate/controller/TranslateController.java
@@ -1,7 +1,10 @@
 package live.dolang.api.translate.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import live.dolang.api.common.response.BaseResponse;
 import live.dolang.api.translate.service.TranslateService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import live.dolang.api.translate.dto.TranslateRequest;
 import live.dolang.api.translate.dto.TranslateResponse;
 
+@Tag(name = "translate")
 @RestController
 @RequestMapping("/api")
 class TranslateController {
@@ -20,8 +24,13 @@ class TranslateController {
         this.translateService = translateService;
     }
 
+    @Operation(
+            summary = "번역",
+            description = "Object로 받은 내용들을 target_lang으로 번역합니다.",
+            security = @SecurityRequirement(name = "BearerAuth") // JWT 인증 적용
+    )
     @PostMapping("/translate")
-    public ResponseEntity<TranslateResponse> translate(@RequestBody TranslateRequest request) {
-        return ResponseEntity.ok(translateService.translate(request));
+    public BaseResponse<TranslateResponse> translate(@RequestBody TranslateRequest request) {
+        return BaseResponse.ok(translateService.translate(request));
     }
 }
