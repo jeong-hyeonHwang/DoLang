fca379c1f269fcf6e8a18c97052e13fed3bc5d8e
이준환 <dlwnsghks0401@gmail.com>
2025-02-14 10:25:54 +0900

    fix(call): chat 기능 view에 추가

--- dolang-frontend/src/app/routes/VoiceCallView.tsx
+++ dolang-frontend/src/app/routes/VoiceCallView.tsx
@@ -3,6 +3,7 @@ import { useStompClientContext } from '../../features/Matching/hooks/useClientCo
 import { CallWaveIndicator } from '../../features/VoiceCall/components/CallWaveIndicator.tsx';
 import { usePeerContext } from '../../features/VoiceCall/hooks/usePeerContext.tsx';
 import { useCallContext } from '../../features/VoiceCall/hooks/useCallContext.tsx';
+import VoiceCallChat from "@/features/VoiceCall/components/VoiceCallChat.tsx";
 
 function VoiceCallView() {
   const { matchingResult } = useStompClientContext();
@@ -11,47 +12,50 @@ function VoiceCallView() {
   const { peerId, callStatus, remotePeerId, audioRef } = usePeerContext();
   // 오디오 태그에 localStream, remoteStream을 바인딩
   return (
-    <div>
-      <div className="call-participants-container">
-        <div>
-          <h2>Your Peer ID: {peerId}</h2>
-          <p>Status: {callStatus}</p>
-          <p>Remote Peer ID: {remotePeerId}</p>
+    <>
+      <div>
+        <div className="call-participants-container">
           <div>
-            <audio ref={audioRef} autoPlay controls />
+            <h2>Your Peer ID: {peerId}</h2>
+            <p>Status: {callStatus}</p>
+            <p>Remote Peer ID: {remotePeerId}</p>
+            <div>
+              <audio ref={audioRef} autoPlay controls />
+            </div>
+            <div></div>
           </div>
-          <div></div>
         </div>
+
+        {/* 로컬 오디오 */}
+
+        {localStream && (
+          <audio
+            autoPlay
+            controls
+            muted
+            ref={(audio) => {
+              if (audio && localStream) {
+                audio.srcObject = localStream;
+              }
+            }}
+          />
+        )}
+
+        {/* 리모트 오디오 */}
+        {remoteStream && (
+          <audio
+            autoPlay
+            controls
+            ref={(video) => {
+              if (video && remoteStream) {
+                video.srcObject = remoteStream;
+              }
+            }}
+          />
+        )}
       </div>
-
-      {/* 로컬 오디오 */}
-
-      {localStream && (
-        <audio
-          autoPlay
-          controls
-          muted
-          ref={(audio) => {
-            if (audio && localStream) {
-              audio.srcObject = localStream;
-            }
-          }}
-        />
-      )}
-
-      {/* 리모트 오디오 */}
-      {remoteStream && (
-        <audio
-          autoPlay
-          controls
-          ref={(video) => {
-            if (video && remoteStream) {
-              video.srcObject = remoteStream;
-            }
-          }}
-        />
-      )}
-    </div>
+      <VoiceCallChat />
+    </>
   );
 }
 
