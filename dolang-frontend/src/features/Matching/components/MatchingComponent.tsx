3e3a68156a1d595dca6d403efdd2882901f51019
isemae <deleted.lee13@gmail.com>
2025-02-15 02:27:50 +0900

    refactor(matching): 매칭 컴포넌트 스타일 수정

--- dolang-frontend/src/features/Matching/components/MatchingComponent.tsx
+++ dolang-frontend/src/features/Matching/components/MatchingComponent.tsx
@@ -1,56 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useStompClientContext } from '../hooks/useClientContext';
 import VoiceCallMatchingIndicator from './VoiceCallMatchingIndicator';
 import { usePeerContext } from '../../VoiceCall/hooks/usePeerContext';
 import Cookies from 'js-cookie';
+import { css } from '@emotion/react';
 
 export const MatchingComponent = () => {
-  const { isConnected, isMatching, matchedUser, connectionError, connect, disconnect, startMatching, cancelMatching } =
+  const { isConnected, isMatching, matchingResult, connectionError, connect, cancelMatching, startMatching } =
     useStompClientContext();
-  const [accessToken, setAccessToken] = useState('');
   const { peerId } = usePeerContext();
+  const [accessToken, setAccessToken] = useState('');
 
   useEffect(() => {
     const token = Cookies.get('access_token');
-    if (!token) return;
-    setAccessToken(token);
+    if (token) {
+      setAccessToken(token);
+    }
   }, []);
 
+  // isConnected가 false인 경우 connect
+  useEffect(() => {
+    if (!isConnected) {
+      connect(accessToken);
+    }
+  }, [isConnected, accessToken, connect]);
+
+  const showMatchingProgress = isMatching;
+  const showCancelButton = isMatching && !matchingResult;
+  const showStartButton = !isMatching && peerId;
+  const showMatchedUser = !!matchingResult?.matchedUser;
+
   return (
-    <div>
-      <div>
-        {peerId ? <div>peerId: {peerId}</div> : <div>peerId: null</div>}
-        {!isConnected ? (
-          <button onClick={() => connect(accessToken)}>Connect</button>
-        ) : (
-          <button onClick={disconnect}>Disconnect</button>
-        )}
-      </div>
-
+    <div className="matching-component-container" css={matchingComponentContainerStyle}>
       {connectionError && <div>{connectionError}</div>}
-
-      <div style={{ margin: '15px 0', color: '#666' }}>
-        Connection status:{' '}
-        {isConnected ? (
-          <span style={{ color: 'green' }}>Connected</span>
-        ) : (
-          <span style={{ color: 'red' }}>Disconnected</span>
-        )}
-      </div>
-
       {isConnected && (
-        <div>
-          {!isMatching && <button onClick={startMatching}>Start Matching</button>}
-          {isMatching && (
+        <div className="matching-component-button-container" css={matchingComponentButtonContainerStyle}>
+          {showMatchingProgress && (
             <div>
               <VoiceCallMatchingIndicator />
-              <button onClick={cancelMatching}>Cancel Matching</button>
+              <span>매칭 중입니다...</span>
             </div>
           )}
-
-          {matchedUser && <div>Successfully matched with user: {matchedUser.username}</div>}
+          {showCancelButton && <button onClick={cancelMatching}>매칭 취소</button>}
+          {showStartButton && !matchingResult && <button onClick={startMatching}>매칭 시작</button>}
+          {!isConnected || (!peerId && <p>연결중입니다...</p>)}
+          {showMatchedUser && <div>{matchingResult?.matchedUser.nickname}님과 매칭되었습니다!</div>}
         </div>
       )}
     </div>
   );
 };
+
+const matchingComponentContainerStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+`;
+
+const matchingComponentButtonContainerStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
