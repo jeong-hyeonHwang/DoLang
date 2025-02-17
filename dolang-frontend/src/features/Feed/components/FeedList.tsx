6d47f4c56e8df68e1d3d2ee020f94a73c9cf9037
isemae <deleted.lee13@gmail.com>
2025-02-17 09:18:37 +0900

    fix(feed): 피드 문장이 제공되지 않았을 경우의 guard를 추가

--- dolang-frontend/src/features/Feed/components/FeedList.tsx
+++ dolang-frontend/src/features/Feed/components/FeedList.tsx
@@ -4,6 +4,7 @@ import { css } from '@emotion/react';
 import { getFeedParticipation } from '../services/feedService.ts';
 import { FeedParticipant } from '../types/FeedParticipantsResponse.type.ts';
 import { useFeedParticipation } from '../hooks/useFeed.ts';
+
 const FeedList = ({ feedId }: { feedId: number }) => {
   const feedListContainerStyle = css`
     padding: 1rem;
@@ -15,11 +16,21 @@ const FeedList = ({ feedId }: { feedId: number }) => {
     border-radius: 0.6rem;
     box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1) inset;
   `;
+
+  if (feedId === undefined) {
+    return (
+      <section className="feed-list-container" css={feedListContainerStyle}>
+        <p>피드 ID가 제공되지 않았습니다.</p>
+      </section>
+    );
+  }
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const { data: feedParticipationData, error: feedParticipationError } = useFeedParticipation({
     feedId: feedId.toString(),
   });
+
   useEffect(() => {
     const fetchFeedParticipation = async () => {
       try {
