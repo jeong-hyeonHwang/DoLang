e69c573066f715cbca4b479b84113dec49800b19
isemae <deleted.lee13@gmail.com>
2025-02-17 17:02:04 +0900

    fix(api): 액세스 토큰이 업데이트된 후에 요청 시의 토큰을 조회하도록 인터셉터 설정 추가

--- dolang-frontend/src/features/Feed/services/myFeedService.ts
+++ dolang-frontend/src/features/Feed/services/myFeedService.ts
@@ -1,21 +1,12 @@
-import axios from 'axios';
+import { instance } from '@/api/config/axiosConfig';
 import { MyFeedRequest, MyFeedResponse } from '../types';
-import Cookies from 'js-cookie';
-
-const accessToken = Cookies.get('access_token');
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${accessToken}`,
-  },
-});
+import { apiInstance } from '@/api/utils/instance';
 
 // 내 피드 요청
 // http://localhost:8100/api/myfeed?page=0&lang=en
 export const getMyFeed = async (params: Partial<MyFeedRequest>): Promise<MyFeedResponse> => {
   try {
-    const response = await instance.get('api/myfeed', { params });
+    const response = await apiInstance.get('api/myfeed', { params });
     console.log(response.data);
     return response.data;
   } catch (error) {
