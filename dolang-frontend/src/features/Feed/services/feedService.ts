13859b4cd70d620b85d06938a397c28bfdc408d4
isemae <deleted.lee13@gmail.com>
2025-02-17 11:08:14 +0900

    feat(feed): 유저 피드 참여 구현

--- dolang-frontend/src/features/Feed/services/feedService.ts
+++ dolang-frontend/src/features/Feed/services/feedService.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { FeedParticipantsRequest, FeedParticipantsResponse, FeedSentenceRequest, FeedSentenceResponse } from '../types';
+import Cookies from 'js-cookie';
+import { FeedVoiceUploadRequest } from '../types/FeedVoiceUploadRequest.type';
+
 const base = import.meta.env.VITE_API_BASE_URL;
+const token = Cookies.get('access_token');
 const instance = axios.create({
   baseURL: base,
   headers: {
@@ -9,11 +13,19 @@ const instance = axios.create({
   },
 });
 
+const multipartInstance = axios.create({
+  baseURL: base,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+
 // 오늘의 피드 요청
 export const getFeed = async (params: FeedSentenceRequest): Promise<FeedSentenceResponse> => {
   try {
     const response = await instance.get('api/feed/today', { params });
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -25,7 +37,6 @@ export const getFeed = async (params: FeedSentenceRequest): Promise<FeedSentence
 export const getFeedParticipation = async (params: FeedParticipantsRequest): Promise<FeedParticipantsResponse> => {
   try {
     const response = await instance.get('api/feed/today/participants', { params });
-    console.log(response.data);
     return response.data.result;
   } catch (error) {
     console.error(error);
@@ -33,9 +44,16 @@ export const getFeedParticipation = async (params: FeedParticipantsRequest): Pro
   }
 };
 
-
 // 피드 참여 요청
-// export const postFeedParticipation = async (params: FeedParticipantsRequest) => {
-//   const response = await instance.post('/today/participants', { params });
-//   return response.data;
-// };
+export const postFeedVoiceUpload = async (params: FeedVoiceUploadRequest) => {
+  try {
+    const formData = new FormData();
+    formData.append('file', params.file); 
+
+    const response = await multipartInstance.post(`/api/post/${params.feedId}/upload`, formData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
