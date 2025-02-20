4f9900818839cd629864bfb2da97924a26d5851e
isemae <deleted.lee13@gmail.com>
2025-02-20 17:14:18 +0900

    fix: 메인 뷰에서의 테스트 컴포넌트 삭제

--- dolang-frontend/src/app/routes/MainView.tsx
+++ dolang-frontend/src/app/routes/MainView.tsx
@@ -1,13 +1,11 @@
 import { NoteComponent } from '@/features/Note/components/NoteComponent';
 import MainViewComponent from './MainView/MainViewComponent';
-import { getPromptQuestion } from '@/features/Prompt/services/promptService';
 
 const MainView = () => {
   return (
     <>
       <NoteComponent />
       <MainViewComponent />
-      <button onClick={() => getPromptQuestion({ interestA: ['sports'], interestB: ['music'] })}>getTopic</button>
     </>
   );
 };
